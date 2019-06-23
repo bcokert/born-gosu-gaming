76f13ab91688158bc4a570cb00c3e8cb24794d62
Brandon Okert <brandon.okert@hootsuite.com>
2019-06-22 22:41:24 -0700

    Updated and tested release building script

--- scripts/build-release.sh
+++ scripts/build-release.sh
@@ -1,14 +1,23 @@
 #! /bin/bash
 # Run this on server to build a new release and prepare it to run, after running scripts/deploy-source
 
-SERVER_DIR="/var/born-gosu-gaming/release"
+ROOT_DIR="/tmp/born-gosu-gaming"
+BUILD_DIR="/tmp/born-gosu-gaming/build"
+RELEASE_DIR="/var/born-gosu-gaming/release"
 
-if [ -d ${SERVER_DIR} ]; then
-    ${SERVER_DIR}/bin/born_gosu_gaming stop
+if [ -d ${RELEASE_DIR} ]; then
+    ${RELEASE_DIR}/bin/born_gosu_gaming stop
 fi
 
+source ${ROOT_DIR}/asdf/asdf.sh
+
+cd ${BUILD_DIR}
 mix local.hex --force
 mix local.rebar --force
 mix deps.get
 
 MIX_ENV=prod mix release
+
+cp -r _build/prod/rel/born_gosu_gaming ${RELEASE_DIR}
+
+${RELEASE_DIR}/bin/born_gosu_gaming start
