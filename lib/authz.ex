d362a1ede1b1cc4530b9a5a48fb11e41a3568cba
Brandon Okert <bcokert@gmail.com>
2021-02-22 11:19:17 -0800

    Fixed authorization for a few commands

--- lib/authz.ex
+++ lib/authz.ex
@@ -5,16 +5,20 @@ defmodule Authz do
   """
 
   def is_admin?(member, guild), do: DiscordQuery.member_has_role?(member, "Admins", guild)
+  def is_mentor?(member, guild), do: DiscordQuery.member_has_role?(member, "Mentors", guild)
   def is_member?(member, guild), do: DiscordQuery.member_has_role?(member, "Born Gosu", guild)
 
   def authorized_for_command?(_, _, _, "help"), do: true
+
+  def authorized_for_command?(member, guild, "help", "mentor"), do: is_admin?(member, guild) or is_mentor?(member, guild)
+  def authorized_for_command?(member, guild, "users", "mentor"), do: is_admin?(member, guild) or is_mentor?(member, guild)
 
   def authorized_for_command?(member, guild, "help", "admin"), do: is_admin?(member, guild)
   def authorized_for_command?(member, guild, "setdaylightsavings", "admin"), do: is_admin?(member, guild)
   def authorized_for_command?(member, guild, "daylightsavings", "admin"), do: is_admin?(member, guild)
 
-  def authorized_for_command?(member, guild, "dates", "event"), do: is_admin?(member, guild) or  is_member?(member, guild)
-  def authorized_for_command?(member, guild, "soon", "event"), do: is_admin?(member, guild) or  is_member?(member, guild)
+  def authorized_for_command?(_, _, "dates", "event"), do: true
+  def authorized_for_command?(_, _, "soon", "event"), do: true
   def authorized_for_command?(member, guild, "mine", "event"), do: is_admin?(member, guild) or  is_member?(member, guild)
   def authorized_for_command?(member, guild, "add", "event"), do: is_admin?(member, guild) or  is_member?(member, guild)
   def authorized_for_command?(member, guild, "remove", "event"), do: is_admin?(member, guild) or  is_member?(member, guild)
