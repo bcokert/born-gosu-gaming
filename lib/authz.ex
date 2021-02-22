2321992ef8f0bb1f269ed0699e53ae393b213c36
Brandon Okert <bcokert@gmail.com>
2021-02-22 13:03:20 -0800

    Fixed another missing authorization

--- lib/authz.ex
+++ lib/authz.ex
@@ -1,4 +1,5 @@
 defmodule Authz do
+  require Logger
 
   @moduledoc """
   Provides a declarative api for authorizing calls based on user or channel.
@@ -17,6 +18,7 @@ defmodule Authz do
   def authorized_for_command?(member, guild, "setdaylightsavings", "admin"), do: is_admin?(member, guild)
   def authorized_for_command?(member, guild, "daylightsavings", "admin"), do: is_admin?(member, guild)
 
+  def authorized_for_command?(_, _, "help", "event"), do: true
   def authorized_for_command?(_, _, "dates", "event"), do: true
   def authorized_for_command?(_, _, "soon", "event"), do: true
   def authorized_for_command?(member, guild, "mine", "event"), do: is_admin?(member, guild) or  is_member?(member, guild)
@@ -24,5 +26,8 @@ defmodule Authz do
   def authorized_for_command?(member, guild, "remove", "event"), do: is_admin?(member, guild) or  is_member?(member, guild)
   def authorized_for_command?(member, guild, "tryout", "event"), do: is_admin?(member, guild)
 
-  def authorized_for_command?(member, guild, _, _), do: is_admin?(member, guild)
+  def authorized_for_command?(member, guild, cmd, namespace) do
+    Logger.info("Detected an unhandled authorization check: #{cmd} in #{namespace} by #{member.user.username}")
+    is_admin?(member, guild)
+  end
 end
